8e2fb7b378eff349e9b8932fc6053e4a4626eca0
NagaPrasanthMadduluri-20 <maddulurinagaprasanth9391@gmail.com>
2025-06-05 15:07:29 +0530

    dashboard logo center

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 export default function Dashboard() {
   return (
     <>
       <img
         src="/ATU_LOGO__OUTLINE_GR-YW_VERT.png"
         alt="Logo"
-        className="h-[590px] w-[780px] ml-[180px]"
+        className="h-[590px] w-[780px] ml-0 md:ml-[180px]"
       />
     </>
   );
